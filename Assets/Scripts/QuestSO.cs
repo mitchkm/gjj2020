bce7f7846b2aaa08239e360c0ba6dd05e24aae76
jthpotz <jthpotz@gmail.com>
2020-02-01 18:10:14 -0800

    Cleans up unneeded SOs and touches up QuestSO

--- Assets/Scripts/QuestSO.cs
+++ Assets/Scripts/QuestSO.cs
@@ -16,9 +16,15 @@ public class QuestSO : ScriptableObject
     [SerializeField]
     private ItemSO reward;
 
+    [SerializeField]
+    private CharacterList redirect;
+
+    [SerializeField]
+    private bool useReward = true;
+
     public string GetResponse () {
         if (response.Count > 0) {
-            return String.Format(response [0], reward.itemName);
+            return String.Format(response [0], useReward ? reward.itemName : redirect.ToString ());
         }
         return "No Dialouge";
     }
